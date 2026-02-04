817de6f6b39cf386d52bf2b2bdf57dfa66c4271b
Ninja <shaq.press@gmail.com>
2026-02-04 04:16:30 +0100

    update epg

--- src/components/player/EPGSearch.jsx
+++ src/components/player/EPGSearch.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { FixedSizeList as List } from 'react-window';
-import { searchChannelsByName, searchProgramsByTitle } from '../../db/ProgramQueries';
+import { searchChannelsByName, searchProgramsByTitle } from '../../database/ProgramQueries';
 
 // ============================================================================
 // EPG SEARCH - Search live channels and programs
