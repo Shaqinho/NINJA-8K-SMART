b9b2dd35ac3b1afb36a199bdfec5aaff1d2c5efa
Ninja <shaq.press@gmail.com>
2026-02-07 03:50:15 +0100

    0702 0350

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -5,10 +5,5 @@
 export { default as Player } from './Player';
 export { default as PlayerControls } from './PlayerControls';
 export { default as PlayerSettings } from './PlayerSettings';
-export { default as TimeshiftBar } from './TimeshiftBar';
-export { default as CastButton, CastModal } from './CastButton';
-export { default as PiPButton, PiPMiniController } from './PiPManager';
-export { default as MultiGrid } from './MultiGrid';
 export { default as EPGSearch } from './EPGSearch';
-export { default as EPGBar } from './EPGBar';
 export { default as VideoPlayer } from './VideoPlayer';
